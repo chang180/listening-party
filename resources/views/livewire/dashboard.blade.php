41aa68c3dae6c6b6c359e0e04fc71fd9417c61f9
chang180 <chang180@gmail.com>
2024-11-07 15:39:15 +0800

    Episode 4 finished.

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,9 +1,10 @@
 <?php
 
 use Livewire\Volt\Component;
+use App\Models\ListeningParty;
 
 new class extends Component {
-    public straing $name = '';
+    public string $name = '';
     public $startTime;
 
     public function createListeningParty()
@@ -14,13 +15,20 @@ new class extends Component {
         ]);
     }
 
-    public function with() {
+    public function with()
+    {
         return [
             'listening_parties' => ListeningParty::all(),
         ];
     }
 }; ?>
 
-<div>
-    Hello Volt.
+<div class="flex items-center justify-center min-h-screen bg-slate-150">
+    <div class="w-full max-w-lg px-4">
+        <form wire:submit='createListeningParty' class="space-y-6">
+            <x-input wire:mode='name' placeholder='Listening Party Name' />
+            <x-datetime-picker xire:model="startTime" placeholder="Lostening Party Start Time" />
+            <x-button primary>Create Listening Party</x-button>
+        </form>
+    </div>
 </div>
