12ba6d82154f55a793bbc18c3f211e826aab4c24
chang180 <chang180@gmail.com>
2024-11-07 13:14:03 +0800

    Episode 3 finished.

---
+++ app/Models/Podcast.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Episode;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Podcast extends Model
+{
+    /** @use HasFactory<\Database\Factories\PodcastFactory> */
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function episodes(): HasMany
+    {
+        return $this->hasMany(Episode::class);
+    }
+
+    public function listenParties(): HasMany
+    {
+        return $this->hasMany(ListeningParty::class);
+    }
+}
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -3,7 +3,22 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+    public straing $name = '';
+    public $startTime;
+
+    public function createListeningParty()
+    {
+        ListeningParty::create([
+            'name' => 'New Listening Party',
+            'date' => now(),
+        ]);
+    }
+
+    public function with() {
+        return [
+            'listening_parties' => ListeningParty::all(),
+        ];
+    }
 }; ?>
 
 <div>
